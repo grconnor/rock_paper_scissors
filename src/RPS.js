b2f22b54374e1dbc4a7200c85628cd5434b4dc79
Connor Roelofsen <connorroelofsen15@gmail.com>
2020-09-21 20:54:28 +0200

    Cleans up index.html a bit, adds random computer choice generator and changes background color from grey to light blue.

--- src/RPS.js
+++ src/RPS.js
@@ -3,6 +3,17 @@ let computerChosen
 car result = results()
 const displayResult = document.getElementById('results')
 const computerChoice = document.getElementById('computer-choice')
+const randomComputerChoice = Math.floor(Math.random() * (3))
+
+function generatedComputerChoice() {
+  if (randomComputerChoice === 1) {
+    return computerChosen = 'rock'
+  } else if (randomComputerChoice === 2) {
+    return computerChosen = 'paper'
+  } else if (randomComputerChoice === 3) {
+    return computerChosen = 'scissors'
+  }
+}
 
 function results() {
   if(computerChosen === userChosen) {
